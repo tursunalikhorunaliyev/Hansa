b7b57b1062428bdec802f95b62e23ff89627ed5d
tursunalikhorunaliyev <khorunaliyev@gmail.com>
2022-07-27 17:16:10 +0500

    file added

---
+++ lib/providers/stack_provider.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:hansa_app/blocs/menu_events_bloc.dart';
+
+class StackProvider extends ChangeNotifier{
+  List<MenuActions> list = [];
+
+  addStack(MenuActions action){
+
+  }
+}
