6194d8a9b5aef097b21ad6e22493dacc6cad166a
tursunalikhorunaliyev <khorunaliyev@gmail.com>
2022-07-27 08:52:11 +0500

    tursunali push qildi

--- lib/extra/full_registr.dart
+++ lib/extra/full_registr.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hansa_app/blocs/bloc_flip_login.dart';
 import 'package:hansa_app/drawer_widgets/toggle_switcher.dart';
+import 'package:hansa_app/extra/text_field_for_full_reg.dart';
 import 'package:provider/provider.dart';
 
 class FullRegistr extends StatefulWidget {
@@ -77,37 +78,37 @@ class _FullRegistrState extends State<FullRegistr> {
                     const SizedBox(
                       height: 18,
                     ),
-                    textField("Имя*", isTablet ? 45 : 38, isTablet ? 13 : 10,
-                        isTablet ? FontWeight.w600 : FontWeight.normal),
+                    TextFieldForFullRegister(text:  "Имя", height: isTablet ? 45 : 38, size: isTablet ? 13 : 10,
+                     weight:   isTablet ? FontWeight.w600 : FontWeight.normal),
                     const SizedBox(
                       height: 4,
                     ),
-                    textField(
-                        "Фамилия*",
-                        isTablet ? 45 : 38,
-                        isTablet ? 13 : 10,
-                        isTablet ? FontWeight.w600 : FontWeight.normal),
+                    TextFieldForFullRegister(
+                       text: "Фамилия",
+                     height:   isTablet ? 45 : 38,
+                      size:  isTablet ? 13 : 10,
+                       weight:  isTablet ? FontWeight.w600 : FontWeight.normal),
                     const SizedBox(
                       height: 5,
                     ),
-                    textField("Email*", isTablet ? 45 : 38, isTablet ? 13 : 10,
-                        isTablet ? FontWeight.w600 : FontWeight.normal),
+                    TextFieldForFullRegister(text: "Email", height: isTablet ? 45 : 38, size: isTablet ? 13 : 10,
+                    weight:    isTablet ? FontWeight.w600 : FontWeight.normal),
                     const SizedBox(
                       height: 4,
                     ),
-                    textField(
-                        "Контактный тефон*",
-                        isTablet ? 45 : 38,
-                        isTablet ? 15 : 10,
-                        isTablet ? FontWeight.w600 : FontWeight.normal),
+                    TextFieldForFullRegister(
+                     text:   "Контактный тефон",
+                     height:   isTablet ? 45 : 38,
+                      size:  isTablet ? 15 : 10,
+                      weight:  isTablet ? FontWeight.w600 : FontWeight.normal),
                     const SizedBox(
                       height: 4,
                     ),
-                    textField(
-                        "Дата рождения*",
-                        isTablet ? 45 : 38,
-                        isTablet ? 15 : 10,
-                        isTablet ? FontWeight.w600 : FontWeight.normal),
+                    TextFieldForFullRegister(
+                       text: "Дата рождения",
+                      height:  isTablet ? 45 : 38,
+                       size: isTablet ? 15 : 10,
+                      weight:   isTablet ? FontWeight.w600 : FontWeight.normal),
                     const SizedBox(
                       height: 4,
                     ),
@@ -244,7 +245,7 @@ class _FullRegistrState extends State<FullRegistr> {
                         child: Container(
                           alignment: Alignment.center,
                           height: isTablet ? 60 : 46,
-                          width: isTablet ? 535 : 325,
+                          width: isTablet ? 525 : 325,
                           decoration: BoxDecoration(
                             color: const Color(0xFF25b049),
                             borderRadius: BorderRadius.circular(70),
--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,7 @@ class MyApp extends StatelessWidget {
         ],
         child: const MaterialApp(
           debugShowCheckedModeBanner: false,
-          home: WelcomeScreen(),
+          home: HansaZagruzka(),
         ),
       ),
     );
