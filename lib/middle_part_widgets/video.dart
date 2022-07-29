722a8f64927d84b9ee1e7846e23e2cdb0637ff90
Shahzodbek2005 <shahzodbekkomilov2005@gmail.com>
2022-07-29 19:50:28 +0500

    Fillar qabristoni

--- lib/middle_part_widgets/video.dart
+++ lib/middle_part_widgets/video.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:hansa_app/blocs/bloc_change_title.dart';
 import 'package:hansa_app/blocs/menu_events_bloc.dart';
 import 'package:hansa_app/extra/custom_title.dart';
+import 'package:hansa_app/video/bloc_video_api.dart';
+import 'package:hansa_app/video/model_video.dart';
 import 'package:hansa_app/video_section/custom_video_item.dart';
 import 'package:hansa_app/video_section/custom_video_subitem.dart';
 import 'package:provider/provider.dart';
@@ -19,6 +21,9 @@ class Video extends StatelessWidget {
   Widget build(BuildContext context) {
     final menuEventsBloCProvider = Provider.of<MenuEventsBloC>(context);
     final blocChangeTitleProvider = Provider.of<BlocChangeTitle>(context);
+    final token = Provider.of<String>(context);
+    final blocVideoApi = BlocVideoApi(token);
+    blocVideoApi.eventSink.add(ActionVideo.View);
     return Expanded(
       child: Column(
         children: [
@@ -29,34 +34,49 @@ class Video extends StatelessWidget {
           Expanded(
             child: SingleChildScrollView(
               physics: const BouncingScrollPhysics(),
-              child: Column(
-                  children: List.generate(4, (index) {
-                return Column(
-                  children: [
-                    CustomVideoSubItem(onTap: () {
-                      blocChangeTitleProvider.titleSink.add(titles[index]);
-                      menuEventsBloCProvider.eventSink.add(MenuActions.oKompanii);
-                    },title: titles[index]),
-                    SizedBox(
-                      height: 235,
-                      child: ListView.builder(
-                        physics: const BouncingScrollPhysics(),
-                        itemCount: 15,
-                        scrollDirection: Axis.horizontal,
-                        itemBuilder: (context, index) {
-                          return Padding(
-                            padding:
-                                EdgeInsets.only(left: (index == 0) ? 30 : 0),
-                            child: GestureDetector(
-                                onTap: () {},
-                                child: const CustomVideoListItem()),
-                          );
-                        },
-                      ),
-                    ),
-                  ],
-                );
-              })),
+              child: StreamBuilder<VideoMainOne>(
+                  stream: blocVideoApi.dataStream,
+                  builder: (context, snapshot) {
+                    if (snapshot.data == null) {
+                      return const Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    }
+                    return Column(
+                        children: List.generate(
+                            snapshot.data!.videoListData.list.length, (index) {
+                      return Column(
+                        children: [
+                          CustomVideoSubItem(
+                              onTap: () {
+                                blocChangeTitleProvider.titleSink
+                                    .add(titles[index]);
+                                menuEventsBloCProvider.eventSink
+                                    .add(MenuActions.oKompanii);
+                              },
+                              title: snapshot
+                                  .data!.videoListData.list[index].title),
+                          SizedBox(
+                            height: 235,
+                            child: ListView.builder(
+                              physics: const BouncingScrollPhysics(),
+                              itemCount: 15,
+                              scrollDirection: Axis.horizontal,
+                              itemBuilder: (context, index) {
+                                return Padding(
+                                  padding: EdgeInsets.only(
+                                      left: (index == 0) ? 30 : 0),
+                                  child: GestureDetector(
+                                      onTap: () {},
+                                      child: const CustomVideoListItem()),
+                                );
+                              },
+                            ),
+                          ),
+                        ],
+                      );
+                    }));
+                  }),
             ),
           ),
         ],
---
+++ lib/video/model_video.dart
@@ -0,0 +1,78 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+
+class VideoMainOne {
+  bool status;
+ VideoListData videoListData;
+  VideoMainOne({
+    required this.status,
+    required this.videoListData,
+  });
+
+  factory VideoMainOne.fromMap(Map<String, dynamic> map) {
+    return VideoMainOne(
+      status: map['status'] ,
+      videoListData: VideoListData.fromMap(map['data']),
+    );
+  }
+}
+
+class VideoListData {
+  List<VideoMain> list;
+  VideoListData({
+    required this.list,
+  });
+
+  factory VideoListData.fromMap(List<dynamic> map) {
+    return VideoListData(
+      list: map.map((e) => VideoMain.fromMap(e)).toList(),
+    );
+  }
+}
+
+class VideoMain {
+  String title;
+  VideoList data;
+  VideoMain({
+    required this.title,
+    required this.data,
+  });
+
+  factory VideoMain.fromMap(Map<String, dynamic> map) {
+    return VideoMain(
+      title: map['title'],
+      data: VideoList.fromMap(map["data"]),
+    );
+  }
+}
+
+class VideoList {
+  List<VideoDetails> list;
+  VideoList({
+    required this.list,
+  });
+
+  factory VideoList.fromMap(List<dynamic> map) {
+    return VideoList(list: map.map((e) => VideoDetails.fromMap(e)).toList());
+  }
+}
+
+class VideoDetails {
+  String title;
+  String pictureLink;
+  String videoLink;
+  VideoDetails({
+    required this.title,
+    required this.pictureLink,
+    required this.videoLink,
+  });
+
+  factory VideoDetails.fromMap(Map<String, dynamic> map) {
+    return VideoDetails(
+      title: map['title'],
+      pictureLink: map['picture_link'],
+      videoLink: map['video_link'],
+    );
+  }
+}
