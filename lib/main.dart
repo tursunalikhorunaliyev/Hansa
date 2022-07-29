36006fb8b5d05df9cc42ae0daefc7fe0eb3756f6
Diyorbek76832006 <diyorbek76832006@gmail.com>
2022-07-29 03:22:57 -0700

    PDF Viewer

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,7 @@ import 'package:hansa_app/blocs/bloc_change_title.dart';
 import 'package:hansa_app/blocs/bloc_play_video.dart';
 import 'package:hansa_app/blocs/menu_events_bloc.dart';
 import 'package:hansa_app/blocs/voyti_ili_sozdata_bloc.dart';
-import 'package:hansa_app/screens/empty.dart';
-import 'package:hansa_app/screens/hansa_zagruzka.dart';
-import 'package:hansa_app/screens/welcome_screen.dart';
+import 'package:hansa_app/screens/pdf_viewer.dart';
 import 'package:provider/provider.dart';
 
 void main(List<String> args) {
@@ -42,7 +40,7 @@ class MyApp extends StatelessWidget {
         ],
         child: const MaterialApp(
           debugShowCheckedModeBanner: false,
-          home: WelcomeScreen(),
+          home: PDFView(),
         ),
       ),
     );
