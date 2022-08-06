8a6a2af44411466c9582d0731d4e8920fa08a8cc
tursunalikhorunaliyev <khorunaliyev@gmail.com>
2022-08-06 14:45:45 +0500

    ok

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ import 'package:hansa_app/providers/full_registr_provider.dart';
 import 'package:hansa_app/providers/provider_for_flipping/flip_login_provider.dart';
 import 'package:hansa_app/providers/provider_for_flipping/login_clicked_provider.dart';
 import 'package:hansa_app/providers/provider_for_flipping/provider_for_flipping.dart';
+import 'package:hansa_app/screens/empty.dart';
 import 'package:hansa_app/screens/hansa_zagruzka.dart';
 import 'package:hansa_app/blocs/toggle_switcher_bloc.dart';
 import 'package:hansa_app/screens/splash_screen.dart';
@@ -83,7 +84,7 @@ class MyApp extends StatelessWidget {
         ],
         child: const MaterialApp(
           debugShowCheckedModeBanner: false,
-          home: HansaZagruzka(),
+          home: EmptyScreen(),
         ),
       ),
     );
