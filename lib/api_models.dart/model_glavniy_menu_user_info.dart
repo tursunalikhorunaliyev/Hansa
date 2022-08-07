f3d64f0721409c7e7d9972783c0ad0408217260e
Shahzodbek2005 <shahzodbekkomilov2005@gmail.com>
2022-08-07 14:49:52 +0500

    Osmonlik bolalar

---
+++ lib/api_models.dart/model_glavniy_menu_user_info.dart
@@ -0,0 +1,39 @@
+import 'dart:convert';
+
+class ModelGlavniyMenuUserInfoMain {
+  bool status;
+  Data data;
+  ModelGlavniyMenuUserInfoMain({
+    required this.status,
+    required this.data,
+  });
+
+  factory ModelGlavniyMenuUserInfoMain.fromMap(Map<String, dynamic> map) {
+    return ModelGlavniyMenuUserInfoMain(
+      status: map['status'],
+      data: Data.fromMap(map['data']),
+    );
+  }
+}
+
+class Data {
+  String fullname;
+  int score;
+  String ref_link;
+  String link;
+  Data({
+    required this.fullname,
+    required this.score,
+    required this.ref_link,
+    required this.link,
+  });
+
+  factory Data.fromMap(Map<String, dynamic> map) {
+    return Data(
+      fullname: map['fullname'],
+      score: map['score'],
+      ref_link: map['ref_link'],
+      link: map['_link'],
+    );
+  }
+}
---
+++ lib/blocs/bloc_glavniy_menu_user_info.dart
@@ -0,0 +1,37 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:hansa_app/api_models.dart/model_glavniy_menu_user_info.dart';
+import 'package:hansa_app/enums/enum_action_view.dart';
+import 'package:http/http.dart' as http;
+
+class BlocGlavniyMenuUserInfo {
+  final dataController = StreamController<ModelGlavniyMenuUserInfoMain>.broadcast();
+  final eventController = StreamController<EnumActionView>.broadcast();
+
+  StreamSink<ModelGlavniyMenuUserInfoMain> get dataSink => dataController.sink;
+  Stream<ModelGlavniyMenuUserInfoMain> get dataStream => dataController.stream;
+
+  StreamSink<EnumActionView> get eventSink => eventController.sink;
+  Stream<EnumActionView> get eventStream => eventController.stream;
+
+  BlocGlavniyMenuUserInfo(token) {
+    eventStream.listen((event) async {
+      if (event == EnumActionView.view) {
+        dataSink.add(await getData(token));
+      }
+    });
+  }
+
+  Future<ModelGlavniyMenuUserInfoMain> getData(token) async {
+    http.Response response = await http.get(
+      Uri.parse("https://hansa-lab.ru/api/site/user-short-info"),
+      headers: {"token": token},
+    );
+    print(response.statusCode);
+    print(response.body);
+    print("Bloc Glavniy Menu User Info----------------------------------");
+
+    return ModelGlavniyMenuUserInfoMain.fromMap(jsonDecode(response.body));
+  }
+}
