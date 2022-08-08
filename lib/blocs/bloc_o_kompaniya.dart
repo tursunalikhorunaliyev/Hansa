e0ba41f16e6a13871dcd859a3f58945e167c807e
Shahzodbek2005 <shahzodbekkomilov2005@gmail.com>
2022-08-08 15:34:48 +0500

    Kiyk

--- lib/blocs/bloc_o_kompaniya.dart
+++ lib/blocs/bloc_o_kompaniya.dart
@@ -3,22 +3,23 @@ import 'dart:convert';
 
 import 'package:hansa_app/api_models.dart/model_doljnost.dart';
 import 'package:hansa_app/api_models.dart/model_magazin.dart';
+import 'package:hansa_app/api_models.dart/model_o_kompaniya.dart';
 import 'package:hansa_app/enums/enum_action_view.dart';
 import 'package:http/http.dart' as http;
 
 
 
-class BlocMagazin {
-  final dataController = StreamController<ModelMagazinMain>.broadcast();
+class BlocOKompaniya {
+  final dataController = StreamController<ModelOKompaniyaMain>.broadcast();
   final eventController = StreamController<EnumActionView>.broadcast();
 
-  StreamSink<ModelMagazinMain> get dataSink => dataController.sink;
-  Stream<ModelMagazinMain> get dataStream => dataController.stream;
+  StreamSink<ModelOKompaniyaMain> get dataSink => dataController.sink;
+  Stream<ModelOKompaniyaMain> get dataStream => dataController.stream;
 
   StreamSink<EnumActionView> get eventSink => eventController.sink;
   Stream<EnumActionView> get eventStream => eventController.stream;
 
-  BlocMagazin(token) {
+  BlocOKompaniya(token) {
     eventStream.listen((event) async {
       if (event == EnumActionView.view) {
         dataSink.add(await getData(token));
@@ -26,15 +27,15 @@ class BlocMagazin {
     });
   }
 
-  Future<ModelMagazinMain> getData(token) async {
+  Future<ModelOKompaniyaMain> getData(token) async {
     http.Response response = await http.get(
-      Uri.parse("https://hansa-lab.ru/api/dictionary/store"),
+      Uri.parse("http://hansa-lab.ru/api/site/greeting"),
       headers: {"token": token},
     );
     print(response.statusCode);
     print(response.body);
-    print("Bloc Magazin----------------------------------");
+    print("Bloc O Kompaniya----------------------------------");
 
-    return ModelMagazinMain.fromMap(jsonDecode(response.body));
+    return ModelOKompaniyaMain.fromMap(jsonDecode(response.body));
   }
 }
