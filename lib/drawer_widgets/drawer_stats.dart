6d24cc67b9a7ef72344106881bd1057b6971c745
tursunalikhorunaliyev <khorunaliyev@gmail.com>
2022-08-10 15:38:05 +0500

    marge me

--- lib/drawer_widgets/drawer_stats.dart
+++ lib/drawer_widgets/drawer_stats.dart
@@ -30,128 +30,111 @@ class DrawerStats extends StatelessWidget {
                 padding: const EdgeInsets.symmetric(horizontal: 15),
                 child: SizedBox(
                   height: isTablet ? 500 : null,
-                  child: DataTable(
-                    headingRowHeight: 20,
-                    columnSpacing: 10,
-                    dataRowHeight: 31,
-                    horizontalMargin: 1,
-                    columns: [
-                      DataColumn(
-                          label: Text(
-                            "Место",
-                            style: GoogleFonts.montserrat(
-                              fontSize: isTablet ? 14 : 8,
-                              fontWeight: FontWeight.bold,
+                  child: StreamBuilder<RatingTopModel>(
+                      stream: bloc.stream,
+                      builder: (context, snapshot) {
+                        if (snapshot.hasData) {
+                          return DataTable(
+                            headingRowHeight: 20,
+                            columnSpacing: 10,
+                            dataRowHeight: 31,
+                            horizontalMargin: 1,
+                            columns: [
+                              DataColumn(
+                                  label: Text(
+                                    "Место",
+                                    style: GoogleFonts.montserrat(
+                                      fontSize: isTablet ? 14 : 8,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  numeric: false),
+                              DataColumn(
+                                label: Text(
+                                  "Сеть",
+                                  style: GoogleFonts.montserrat(
+                                    fontSize: isTablet ? 14 : 8,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                              DataColumn(
+                                label: Text(
+                                  "Участник",
+                                  style: GoogleFonts.montserrat(
+                                    fontSize: isTablet ? 14 : 8,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                              DataColumn(
+                                label: Text(
+                                  "Баллы",
+                                  style: GoogleFonts.montserrat(
+                                    fontSize: isTablet ? 14 : 8,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ],
+                            rows: List.generate(
+                              10,
+                              (index) => DataRow(
+                                color: MaterialStateProperty.all(
+                                  (index == 9)
+                                      ? const Color(0xffe21a37)
+                                      : Colors.white,
+                                ),
+                                cells: [
+                                  DataCell(
+                                    Text(
+                                      "${index + 1}",
+                                      style: GoogleFonts.montserrat(
+                                          fontSize: isTablet ? 13 : 8,
+                                          fontWeight: FontWeight.normal,
+                                          color: (index == 9)
+                                              ? const Color(0xffffffff)
+                                              : const Color(0xff353433)),
+                                    ),
+                                  ),
+                                  DataCell(Text(
+                                    snapshot.data!.data.list[index].shop_net,
+                                    style: GoogleFonts.montserrat(
+                                        fontSize: isTablet ? 12 : 8,
+                                        fontWeight: FontWeight.normal,
+                                        color: (index == 9)
+                                            ? const Color(0xffffffff)
+                                            : const Color(0xff353433)),
+                                  )),
+                                  DataCell(Text(
+                                    snapshot.data!.data.list[index].name,
+                                    style: GoogleFonts.montserrat(
+                                        fontSize: 8,
+                                        fontWeight: FontWeight.normal,
+                                        color: (index == 9)
+                                            ? const Color(0xffffffff)
+                                            : const Color(0xff353433)),
+                                  )),
+                                  DataCell(Text(
+                                    snapshot.data!.data.list[index].score,
+                                    style: GoogleFonts.montserrat(
+                                        fontSize: 8,
+                                        fontWeight: FontWeight.normal,
+                                        color: (index == 9)
+                                            ? const Color(0xffffffff)
+                                            : const Color(0xff353433)),
+                                  )),
+                                ],
+                              ),
                             ),
-                          ),
-                          numeric: false),
-                      DataColumn(
-                        label: Text(
-                          "Сеть",
-                          style: GoogleFonts.montserrat(
-                            fontSize: isTablet ? 14 : 8,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                      DataColumn(
-                        label: Text(
-                          "Участник",
-                          style: GoogleFonts.montserrat(
-                            fontSize: isTablet ? 14 : 8,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                      DataColumn(
-                        label: Text(
-                          "Баллы",
-                          style: GoogleFonts.montserrat(
-                            fontSize: isTablet ? 14 : 8,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                    ],
-                    rows: List.generate(
-                      10,
-                      (index) => DataRow(
-                        color: MaterialStateProperty.all(
-                          (index == 9) ? const Color(0xffe21a37) : Colors.white,
-                        ),
-                        cells: [
-                          DataCell(
-                            Text(
-                              "${index + 1}",
-                              style: GoogleFonts.montserrat(
-                                  fontSize: isTablet ? 13 : 8,
-                                  fontWeight: FontWeight.normal,
-                                  color: (index == 9)
-                                      ? const Color(0xffffffff)
-                                      : const Color(0xff353433)),
-                            ),
-                          ),
-                          DataCell(
-                            StreamBuilder<RatingTopModel>(
-                                stream: bloc.stream,
-                                builder: (context, snapshot) {
-                                  if (snapshot.hasData) {
-                                    return Text(
-                                      snapshot.data!.data.list[index].shop_net,
-                                      style: GoogleFonts.montserrat(
-                                          fontSize: isTablet ? 12 : 8,
-                                          fontWeight: FontWeight.normal,
-                                          color: (index == 9)
-                                              ? const Color(0xffffffff)
-                                              : const Color(0xff353433)),
-                                    );
-                                  } else {
-                                    return const Center(child: SizedBox());
-                                  }
-                                }),
-                          ),
-                          DataCell(
-                            StreamBuilder<RatingTopModel>(
-                                stream: bloc.stream,
-                                builder: (context, snapshot) {
-                                  if (snapshot.hasData) {
-                                    return Text(
-                                      snapshot.data!.data.list[index].name,
-                                      style: GoogleFonts.montserrat(
-                                          fontSize: 8,
-                                          fontWeight: FontWeight.normal,
-                                          color: (index == 9)
-                                              ? const Color(0xffffffff)
-                                              : const Color(0xff353433)),
-                                    );
-                                  } else {
-                                    return const Center(child: SizedBox());
-                                  }
-                                }),
-                          ),
-                          DataCell(
-                            StreamBuilder<RatingTopModel>(
-                                stream: bloc.stream,
-                                builder: (context, snapshot) {
-                                  if (snapshot.hasData) {
-                                    return Text(
-                                      snapshot.data!.data.list[index].score,
-                                      style: GoogleFonts.montserrat(
-                                          fontSize: 8,
-                                          fontWeight: FontWeight.normal,
-                                          color: (index == 9)
-                                              ? const Color(0xffffffff)
-                                              : const Color(0xff353433)),
-                                    );
-                                  } else {
-                                    return const Center(child: SizedBox());
-                                  }
-                                }),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
+                          );
+                        } else {
+                          return SpinKitPulse(
+                            color: Color(0xffe21a37),
+                          );
+                        }
+                        ;
+                      }),
                 )),
             Padding(
               padding: const EdgeInsets.all(10.0),
