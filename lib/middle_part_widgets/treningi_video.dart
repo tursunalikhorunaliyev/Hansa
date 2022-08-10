b7d94950db757431595e10f42b75a66d4d3e24a2
Umar763 <umarnematovv98@gmail.com>
2022-08-10 10:54:22 +0500

    pushshshsshhshshs

--- lib/middle_part_widgets/treningi_video.dart
+++ lib/middle_part_widgets/treningi_video.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:hansa_app/extra/custom_clip_item.dart';
+import 'package:hansa_app/extra/custom_tablet_stati_item.dart';
 import 'package:hansa_app/extra/custom_title.dart';
 import 'package:hansa_app/training_video_section/custom_tablet_photos.dart';
 import 'package:hansa_app/training_video_section/custom_treningi_photos.dart';
 import 'package:provider/provider.dart';
 import 'package:sticky_headers/sticky_headers/widget.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class TreningiVideo extends StatelessWidget {
   const TreningiVideo({Key? key}) : super(key: key);
@@ -38,26 +40,53 @@ class TreningiVideo extends StatelessWidget {
                 imagePath: "assets/kak_title.png",
                 title: "Похожие мероприятия",
               ),
-              content: Column(
-                children: List.generate(
-                  15,
-                  (index) => Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      CustomClipItem(
-                        onTap: () {},
-                        backgroundColor: const Color(0xff000004),
-                        buttonColor: const Color(0xffe21a37),
-                        buttonTextColor: const Color(0xffffffff),
-                        titleColor: const Color(0xffffffff),
-                        buttonText: "Смотреть",
-                        title:
-                            "Иммерсивное шоу \"Увидимся\nна кухне\" г. Санкт-Петербург:\n06/06/2",
+              content: isTablet
+                  ? Column(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 11.w),
+                          child: GridView(
+                            physics: const BouncingScrollPhysics(),
+                            shrinkWrap: true,
+                            gridDelegate:
+                                const SliverGridDelegateWithFixedCrossAxisCount(
+                                    crossAxisCount: 2,
+                                    childAspectRatio: 10 / 3),
+                            children: List.generate(
+                              30,
+                              (index) => CustomStatiTabletItem(
+                                  onTap: () {},
+                                  backgroundColor: const Color(0xff000004),
+                                  buttonColor: const Color(0xffe21a37),
+                                  buttonTextColor: const Color(0xffffffff),
+                                  titleColor: const Color(0xffffffff),
+                                  buttonText: "Смотреть",
+                                  title:
+                                      "Иммерсивное шоу \"Увидимся\nна кухне\" г. Санкт-Петербург:\n06/06/2"),
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
+                  : Column(
+                      children: List.generate(
+                        15,
+                        (index) => Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            CustomClipItem(
+                                onTap: () {},
+                                backgroundColor: const Color(0xff000004),
+                                buttonColor: const Color(0xffe21a37),
+                                buttonTextColor: const Color(0xffffffff),
+                                titleColor: const Color(0xffffffff),
+                                buttonText: "Смотреть",
+                                title:
+                                    "Иммерсивное шоу \"Увидимся\nна кухне\" г. Санкт-Петербург:\n06/06/2"),
+                          ],
+                        ),
                       ),
-                    ],
-                  ),
-                ),
-              ),
+                    ),
             ),
           ],
         ),
--- lib/training_video_section/custom_tablet_photos.dart
+++ lib/training_video_section/custom_tablet_photos.dart
@@ -8,172 +8,181 @@ class TabletPhotosItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final page = PageController(initialPage: 0);
-    final isTablet = Provider.of<bool>(context);
 
     return Padding(
-      padding: const EdgeInsets.only(bottom: 11),
+      padding: const EdgeInsets.only(bottom: 11, left: 25, right: 25),
       child: SizedBox(
-        width: isTablet ? 655 : 345,
+        width: double.infinity,
         child: Stack(
           children: [
             Column(
               children: [
                 Stack(
                   children: [
-                    Center(
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(8),
-                        child: ConstrainedBox(
-                          constraints: BoxConstraints(
-                            maxHeight: 156,
-                            maxWidth: isTablet ? 675 : 325,
-                          ),
-                          child: PageView.builder(
-                            allowImplicitScrolling: true,
-                            physics: const BouncingScrollPhysics(),
-                            controller: page,
-                            itemCount: 5,
-                            itemBuilder: (context, index) {
-                              return Center(
-                                child: SizedBox(
-                                  height: 156,
-                                  width: isTablet ? 695 : 325,
-                                  child: Image.asset(
-                                    "assets/tt.png",
-                                    fit: BoxFit.cover,
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: Center(
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(8),
+                          child: ConstrainedBox(
+                            constraints: BoxConstraints(
+                                maxHeight: 520, maxWidth: double.infinity),
+                            child: PageView.builder(
+                              allowImplicitScrolling: true,
+                              physics: const BouncingScrollPhysics(),
+                              controller: page,
+                              itemCount: 5,
+                              itemBuilder: (context, index) {
+                                return Center(
+                                  child: SizedBox(
+                                    height: 520,
+                                    width: double.infinity,
+                                    child: Image.asset(
+                                      "assets/tt.png",
+                                      fit: BoxFit.cover,
+                                    ),
                                   ),
-                                ),
-                              );
-                            },
+                                );
+                              },
+                            ),
                           ),
                         ),
                       ),
                     ),
-                    Row(
-                      children: [
-                        const SizedBox(
-                          height: 156,
-                        ),
-                        GestureDetector(
-                          onTap: () {
-                            if (page.hasClients && !(page.offset < 325)) {
-                              page.animateTo(
-                                page.offset - 300,
-                                duration: const Duration(milliseconds: 200),
-                                curve: Curves.bounceIn,
-                              );
-                            }
-                          },
-                          child: Container(
-                            height: 35,
-                            width: 27,
-                            decoration: const BoxDecoration(
-                              borderRadius: BorderRadius.only(
-                                topRight: Radius.circular(10),
-                                bottomRight: Radius.circular(10),
-                                topLeft: Radius.circular(16),
-                                bottomLeft: Radius.circular(16),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 225),
+                      child: Row(
+                        children: [
+                          GestureDetector(
+                            onTap: () {
+                              if (page.hasClients && !(page.offset < 325)) {
+                                page.animateTo(
+                                  page.offset - 300,
+                                  duration: const Duration(milliseconds: 200),
+                                  curve: Curves.bounceIn,
+                                );
+                              }
+                            },
+                            child: Container(
+                              height: 80,
+                              width: 60,
+                              decoration: const BoxDecoration(
+                                borderRadius: BorderRadius.only(
+                                  topRight: Radius.circular(23),
+                                  bottomRight: Radius.circular(23),
+                                  topLeft: Radius.circular(35),
+                                  bottomLeft: Radius.circular(35),
+                                ),
+                                color: Color(0xffff163e),
                               ),
-                              color: Color(0xffff163e),
-                            ),
-                            child: Center(
-                              child: Image.asset(
-                                "assets/back.png",
-                                height: 20,
-                                width: 20,
-                              ),
-                            ),
-                          ),
-                        ),
-                        const Spacer(),
-                        GestureDetector(
-                          onTap: () {
-                            if (page.hasClients && page.offset < 4 * 325) {
-                              page.animateTo(
-                                page.offset + 300,
-                                duration: const Duration(milliseconds: 200),
-                                curve: Curves.ease,
-                              );
-                            }
-                          },
-                          child: Container(
-                            height: 35,
-                            width: 27,
-                            decoration: const BoxDecoration(
-                              borderRadius: BorderRadius.only(
-                                topLeft: Radius.circular(10),
-                                bottomLeft: Radius.circular(10),
-                                topRight: Radius.circular(16),
-                                bottomRight: Radius.circular(16),
-                              ),
-                              color: Color(0xffff163e),
-                            ),
-                            child: Center(
-                              child: Image.asset(
-                                "assets/forward.png",
-                                height: 20,
-                                width: 20,
+                              child: Center(
+                                child: Image.asset(
+                                  "assets/back.png",
+                                  height: 45,
+                                  width: 45,
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                      ],
+                          const Spacer(),
+                          GestureDetector(
+                            onTap: () {
+                              if (page.hasClients && page.offset < 4 * 325) {
+                                page.animateTo(
+                                  page.offset + 300,
+                                  duration: const Duration(milliseconds: 200),
+                                  curve: Curves.ease,
+                                );
+                              }
+                            },
+                            child: Container(
+                              height: 80,
+                              width: 60,
+                              decoration: const BoxDecoration(
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(23),
+                                  bottomLeft: Radius.circular(23),
+                                  topRight: Radius.circular(35),
+                                  bottomRight: Radius.circular(35),
+                                ),
+                                color: Color(0xffff163e),
+                              ),
+                              child: Center(
+                                child: Image.asset(
+                                  "assets/forward.png",
+                                  height: 45,
+                                  width: 45,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ],
                 ),
                 const SizedBox(
                   height: 11,
                 ),
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(8),
-                  child: Container(
-                    height: 90,
-                    width: 325,
-                    color: const Color(0xffffffff),
-                    child: Row(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(left: 15),
-                          child: Text(
-                            "Иммерсивное шоу\n\"Увидимся на кухне\"г.\nЕкатеринбург:     18.04.2019",
-                            style: GoogleFonts.montserrat(
-                              fontSize: 13,
-                              fontWeight: FontWeight.bold,
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(8),
+                    child: Container(
+                      height: 150,
+                      padding: EdgeInsets.only(right: 20, left: 20),
+                      width: double.infinity,
+                      color: const Color(0xffffffff),
+                      child: Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(
+                                left: 15, top: 10, bottom: 8),
+                            child: SizedBox(
+                              width: 550,
+                              child: Text(
+                                "Иммерсивное шоу\"Увидимся на кухfaisudghsalkjjfhnasdoifhosdijfasdofhijsdoifjasdoidfjasdfoiijsaddflasjkddfalsdjfa;sldfjwe4oujwepofjuwepfojsdflskmd.,zsdm  mczlx,mcsojdfweapoafkjkaseorkjwewefpokasd;flkasdfl;kasdfokawepofkaweofkaseoefkjasdpofkasdpofkjasdpdofjweroijfseoifjsdaofjsdoijfasdofijasdijfdfoifgjiodasdiufhsdidofhasdodfihasdfoihsdafoihsaddfoiashdfoiashdfaosidhfasdoifhasdoifihasdoifhasdofiihasdoifhasdoifhasdoifhasdoifhasdoifhasdoifhasdo;ifhasdoifhasdo;ifhasdoidfhне\"г.Екатеринбург:     18.04.2019",
+                                overflow: TextOverflow.fade,
+                                style: GoogleFonts.montserrat(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
                             ),
                           ),
-                        ),
-                        const Spacer(),
-                        Padding(
-                          padding: const EdgeInsets.only(right: 8),
-                          child: GestureDetector(
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(64),
-                              child: Container(
-                                height: 25,
-                                width: 95,
-                                color: const Color(0xffff163e),
-                                child: Center(
-                                  child: Text(
-                                    "Скачать",
-                                    style: GoogleFonts.montserrat(
-                                      fontSize: 10,
-                                      color: const Color(0xffffffff),
+                          const Spacer(),
+                          Padding(
+                            padding: const EdgeInsets.only(right: 35),
+                            child: GestureDetector(
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(64),
+                                child: Container(
+                                  height: 35,
+                                  width: 130,
+                                  color: const Color(0xffff163e),
+                                  child: Center(
+                                    child: Text(
+                                      "Скачать",
+                                      style: GoogleFonts.montserrat(
+                                        fontSize: 15,
+                                        color: const Color(0xffffffff),
+                                      ),
                                     ),
                                   ),
                                 ),
                               ),
                             ),
-                          ),
-                        )
-                      ],
+                          )
+                        ],
+                      ),
                     ),
                   ),
                 ),
               ],
             ),
             Positioned(
-              top: 131,
-              right: 25,
+              top: 500,
+              right: 110,
               child: Container(
                 height: 55,
                 width: 55,
@@ -185,7 +194,7 @@ class TabletPhotosItem extends StatelessWidget {
                   child: Icon(
                     Icons.favorite,
                     color: Color(0xffed3851),
-                    size: 20,
+                    size: 35,
                   ),
                 ),
               ),
