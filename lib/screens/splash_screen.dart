c6d68de63c8fd0785643954c7ab3bed385c0b38b
Umar763 <umarnematovv98@gmail.com>
2022-08-11 09:08:06 +0500

    у

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -2,10 +2,13 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:hansa_app/screens/hansa_zagruzka.dart';
 import 'package:hansa_app/screens/welcome_screen.dart';
 import 'package:hive/hive.dart';
 import 'package:provider/provider.dart';
+
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:http/http.dart' as http;
 
 class SplashScreen extends StatefulWidget {
@@ -26,11 +29,12 @@ class _SplashScreenState extends State<SplashScreen> {
 
   checkUser() async {
     if (box.get("isSaved") != null && box.get("isSaved")! as bool) {
-      Map<String, dynamic> map = await login(box.get("username"), box.get("password"));
+      Map<String, dynamic> map =
+          await login(box.get("username"), box.get("password"));
       await Future.delayed(const Duration(seconds: 1));
       goToHome(await map["data"]["token"]);
     } else {
-      await Future.delayed(const Duration(seconds: 0));
+      await Future.delayed(const Duration(seconds: 2));
       goToHansaZagruzka();
     }
   }
@@ -44,19 +48,59 @@ class _SplashScreenState extends State<SplashScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    final isTablet = Provider.of<bool>(context);
+    return Scaffold(
       body: Center(
-          child: SpinKitWanderingCubes(
-        color: Colors.red,
-      )),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              "assets/logoHansa.png",
+              height: 50,
+              width: 300,
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: isTablet ? 20.h : 24.h),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "#Увидимся",
+                    style: GoogleFonts.montserrat(
+                        color: const Color.fromARGB(255, 59, 59, 59),
+                        fontSize: isTablet ? 16.sp : 23.sp,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    "на",
+                    style: GoogleFonts.montserrat(
+                        color: const Color.fromARGB(255, 59, 59, 59),
+                        fontSize: isTablet ? 16.sp : 23.sp,
+                        fontWeight: FontWeight.w500),
+                  ),
+                  Text(
+                    "кухне",
+                    style: GoogleFonts.montserrat(
+                        color: const Color.fromARGB(255, 59, 59, 59),
+                        fontSize: isTablet ? 16.sp : 23.sp,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 
-  goToHome(token){
-    Navigator.push(context,
-        MaterialPageRoute(builder: (context) => Provider(
-          create: (context) => token.toString(),
-          child: const WelcomeScreen())));
+  goToHome(token) {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => Provider(
+                create: (context) => token.toString(),
+                child: const WelcomeScreen())));
   }
 
   goToHansaZagruzka() {
