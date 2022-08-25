a5daaa7a9e5dd28eb4ec6a92a1f252b93b2c306e
tursunalikhorunaliyev <khorunaliyev@gmail.com>
2022-08-25 14:17:25 +0500

    ok

--- lib/download_exists.dart
+++ lib/download_exists.dart
@@ -4,7 +4,6 @@ import 'dart:io';
 
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:image_gallery_saver/image_gallery_saver.dart';
 import 'package:path_provider/path_provider.dart';
 
 class ExistsCheck extends StatefulWidget {
