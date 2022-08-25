48caffeaa7c96f5e96254f32e8f43c321cab5543
Shahzodbek2005 <shahzodbekkomilov2005@gmail.com>
2022-08-25 19:01:18 +0500

    HHHHHHH

--- lib/training_video_section/custom_tablet_photos.dart
+++ lib/training_video_section/custom_tablet_photos.dart
@@ -1,6 +1,10 @@
 import 'dart:developer';
 import 'dart:io';
 //Salom
+//Salom
+//Salom
+//Salom
+//Salom
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
