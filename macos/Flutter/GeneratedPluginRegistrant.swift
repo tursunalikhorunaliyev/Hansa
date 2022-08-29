0307b35205395fab8c798aaf4d53974784ee10f7
BaxtiyorjonAbduqahhorov <baxtiyorjonabduqahhorov2004@gmail.com>
2022-08-29 11:34:58 +0500

    notification added #4

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,12 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import audio_session
-<<<<<<< HEAD
 import firebase_core
 import firebase_messaging
 import flutter_local_notifications
-=======
->>>>>>> 6ab9c1e533afd2a1960fd06810f16b7db85fb96a
 import just_audio
 import path_provider_macos
 import share_plus_macos
@@ -21,12 +18,9 @@ import wakelock_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
-<<<<<<< HEAD
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
-=======
->>>>>>> 6ab9c1e533afd2a1960fd06810f16b7db85fb96a
   JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
