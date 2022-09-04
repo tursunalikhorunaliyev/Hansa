145602b1bbd24a0ed77fc0c6634dcae08b24da10
tursunalikhorunaliyev <khorunaliyev@gmail.com>
2022-09-04 10:57:58 +0500

    ok pushshed

--- lib/classes/notification_token.dart
+++ lib/classes/notification_token.dart
@@ -1,4 +1,5 @@
 import 'dart:developer';
+import 'dart:io';
 
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:http/http.dart';
@@ -6,6 +7,7 @@ import 'package:http/http.dart';
 class NotificationToken {
   Future<String> getToken() async {
     final fcmToken = await FirebaseMessaging.instance.getToken();
+    
     Map<String, String> body = {"token": fcmToken.toString()};
     await post(
       Uri.parse("https://hansa-lab.ru/api/auth/register-token"),
